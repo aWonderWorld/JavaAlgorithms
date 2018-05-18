fc0beaa6c53b8a64dec0c2b0e06aeece621fc574
name <jojofu1993@outlook.com>
2018-05-18 09:28:02 +0800

    reset

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
 
+import edu.princeton.cs.algs4.StdIn;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	   StdIn.readInt();
     }
 }
